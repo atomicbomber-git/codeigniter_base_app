967064710bf88aab3568dfd15db5ab8f17716c53
James Patrick Keegan <atomicbomber.email@gmail.com>
2019-04-20 20:14:09 +0700

    Updates.

--- application/app/BaseController.php
+++ application/app/BaseController.php
@@ -36,4 +36,65 @@ class BaseController extends \CI_Controller
 
         $this->{$method}(...$args);
     }
+
+    public function validate($rules)
+    {        
+        foreach ($rules as $rule) {
+
+            if (strpos($rule[0], "*")) {
+                $field = explode('[', $rule[0])[0];
+                $data = $this->input->get_post($field);
+
+                if (null === $data) {
+                    continue;
+                }
+
+                $rule_template = $rule[0];
+
+                foreach (array_keys($data) as $key) {
+                    $rule[0] = str_replace("*", $key, $rule_template);
+                    $this->form_validation->set_rules(...$rule);
+                }
+            }
+            else {
+                $this->form_validation->set_rules(...$rule);
+            }
+        }
+
+        if ($this->form_validation->run()) {
+            return;
+        }
+
+        // If regular request
+        if (! $this->input->is_ajax_request()) {
+            $this->session->set_flashdata('errors', $this->form_validation->error_array());
+            $this->saveOldFormData();
+            $this->redirectBack();
+        }
+        else {
+            http_response_code(422);
+            $this->jsonResponse(['data' => [
+                'message' => 'Terdapat kesalahan dalam data.',
+                'errors' => $this->form_validation->error_array()
+            ]]);
+        }
+    }
+
+    protected function saveOldFormData()
+    {
+        $this->session->set_flashdata('old', $this->input->get_post(null));
+    }
+
+    protected function redirectBack()
+    {
+        redirect($_SERVER['HTTP_REFERER'], 'refresh');
+        exit;
+    }
+
+    public function authorize()
+    {
+        if (!Auth::check()) {
+            redirect(base_url("login"));
+        }
+    }
 }
